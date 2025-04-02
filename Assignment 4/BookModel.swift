fdddefc5a34f873bb5c1c9be5fc3f8e902073389
Alex-Seda <alexseda04@outlook.com>
2025-04-02 15:24:22 -0500

    Created BookModel

---
+++ Assignment 4/BookModel.swift
@@ -0,0 +1,18 @@
+//
+//  BookModel.swift
+//  Assignment 4
+//
+//  Created by Alex on 4/2/25.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct Book {
+    let thumbnail: Image
+    let title: String
+    let author: String
+    let editor: String
+    var favorite: Bool
+}
