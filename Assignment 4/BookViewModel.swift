38d77ea03c0897ed11bde6f123ecd6d0bbb943f3
Alex-Seda <alexseda04@outlook.com>
2025-04-04 12:18:36 -0500

    Add API functionality that returns JSON

--- Assignment 4/BookViewModel.swift
+++ Assignment 4/BookViewModel.swift
@@ -9,8 +9,67 @@ import Foundation
 
 class BookViewModel {
     var books: [Book]
+    var jsonData: [BookResult]
+    let url = "https://openlibrary.org/search.json?q=the+lord+of+the+rings"
     
     init(){
         books = [] // Populate books array from CoreData
+        jsonData = []
+        jsonData = getData(from: url)
     }
+    
+    private func getData(from url: String) -> [BookResult]{
+        var rawData: [BookResult]
+        rawData = []
+        
+        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            guard let data = data, error == nil else {
+                print("Something went wrong")
+                return
+            }
+        
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch {
+                print("Failed to convert JSON to struct")
+            }
+            
+            guard let json = result else{
+                return
+            }
+            
+            /*
+            print(json.docs[0].title)
+            print(json.docs[0].author_name)
+            print(json.docs[0].first_publish_year)
+             */
+            
+            rawData = json.docs
+        })
+        
+        task.resume() // Run the task
+        
+        return rawData
+    }
+    
+    private func jsonToCore(raw:[BookResult]) -> [Book]{
+        
+    }
+    
+    func search(query: String){
+        let newURL = url+query
+        jsonData = getData(from: newURL)
+    }
+}
+
+struct Response: Codable{
+    let docs: [BookResult]
+}
+
+struct BookResult: Codable {
+    let title: String
+    let author_name: [String]?
+    let first_publish_year: Int?
 }
